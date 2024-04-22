5306c0a706323813bd180fee289f963470b97e47
santoshguru-11 <d.vns.gurusantosh11@gmail.com>
2024-04-22 21:58:12 +0530

    arr_3

---
+++ access_arr.py
@@ -0,0 +1,7 @@
+import array as arr
+a = arr.array('i', [1, 2, 3, 4, 5, 6])
+print("Access element is: ", a[0])
+print("Access element is: ", a[3])
+b = arr.array('d', [2.5, 3.2, 3.3])
+print("Access element is: ", b[1])
+print("Access element is: ", b[2])
